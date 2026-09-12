5c27e8b4dd2c87af3435b8027ac110dd864cf2f2
Hirota Satoshi <202610880@edu.nishiyamato.ed.jp>
2026-09-12 19:00:46 +0900

    修正: ts-node不要のJest設定を追加

---
+++ jest.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('jest').Config} */
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  collectCoverageFrom: ['lib/**/*.ts'],
+}
+
+module.exports = config
