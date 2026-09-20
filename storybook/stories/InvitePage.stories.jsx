7cb0a2a0a9bfd6f37ac425a394dc2e65a808b462
Hirota Satoshi <202610880@edu.nishiyamato.ed.jp>
2026-09-21 08:56:44 +0900

    test(ui): add referral page stories

---
+++ storybook/stories/InvitePage.stories.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import InvitePage from '../../app/(player)/invite/page'
+
+const meta = {
+  title: 'Pages/Account/Invite',
+  component: InvitePage,
+  parameters: { nextjs: { navigation: { pathname: '/invite' } } },
+}
+
+export default meta
+
+export const LoggedOut = {
+  parameters: { mockApi: { 'GET /api/referral/me': { status: 401, body: { error: 'unauthorized' } } } },
+}
+
+export const ReferralReady = {
+  parameters: {
+    mockApi: {
+      'GET /api/referral/me': {
+        body: {
+          referral_code: 'STORYBOOK42',
+          invited_count: 3,
+          invited_at: ['2026-09-01T00:00:00.000Z', '2026-09-08T00:00:00.000Z', '2026-09-15T00:00:00.000Z'],
+        },
+      },
+    },
+  },
+}
