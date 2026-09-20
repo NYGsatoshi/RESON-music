ae38994b6ebafcb35ad01bcaf0ebc567c62dc9f2
Hirota Satoshi <202610880@edu.nishiyamato.ed.jp>
2026-09-21 08:56:49 +0900

    test(ui): add artist supporters stories

---
+++ storybook/stories/SupportersPage.stories.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import SupportersPage from '../../app/(artist)/supporters/page'
+
+const meta = {
+  title: 'Pages/Artist/Supporters',
+  component: SupportersPage,
+  parameters: { nextjs: { navigation: { pathname: '/supporters' } } },
+}
+
+export default meta
+
+export const Empty = {
+  parameters: { mockApi: { 'GET /api/artist/supporters': { body: { supporters: [] } } } },
+}
+
+export const WithSupporters = {
+  parameters: {
+    mockApi: {
+      'GET /api/artist/supporters': {
+        body: { supporters: [
+          { user_id: 'supporter-1', display_name: 'Listener One', heart_count: 8, boost_count: 2, tip_total_yen: 1200 },
+          { user_id: 'supporter-2', display_name: null, heart_count: 3, boost_count: 0, tip_total_yen: 0 },
+        ] },
+      },
+    },
+  },
+}
