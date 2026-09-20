3c0022fb9d5b56d2b94ef76bcf41aef7284e5767
Hirota Satoshi <202610880@edu.nishiyamato.ed.jp>
2026-09-21 08:42:16 +0900

    test(ui): add communities page stories

---
+++ storybook/stories/CommunitiesPage.stories.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import CommunitiesPage from '../../app/(player)/communities/page'
+
+const meta = {
+  title: 'Pages/Social/Communities',
+  component: CommunitiesPage,
+  parameters: {
+    nextjs: { navigation: { pathname: '/communities' } },
+  },
+}
+
+export default meta
+
+export const MixedMembership = {
+  parameters: {
+    mockApi: {
+      'GET /api/communities': {
+        body: {
+          communities: [
+            {
+              id: 'genre-rock',
+              name: 'Rock',
+              parent_id: null,
+              member_count: 128,
+              joined: true,
+            },
+            {
+              id: 'genre-jazz',
+              name: 'Jazz',
+              parent_id: null,
+              member_count: 64,
+              joined: false,
+            },
+          ],
+        },
+      },
+      'POST /api/communities': { body: { ok: true } },
+      'DELETE /api/communities': { body: { ok: true } },
+    },
+  },
+}
