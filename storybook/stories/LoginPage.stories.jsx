14cbcc215f1a2eed5965859d9644174c4dec5b8a
Hirota Satoshi <202610880@edu.nishiyamato.ed.jp>
2026-09-21 08:04:02 +0900

    test(ui): add login page stories

---
+++ storybook/stories/LoginPage.stories.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import LoginPage from '../../app/(auth)/login/page'
+
+const meta = {
+  title: 'Pages/Auth/Login',
+  component: LoginPage,
+  parameters: {
+    nextjs: {
+      navigation: {
+        pathname: '/login',
+      },
+    },
+  },
+}
+
+export default meta
+
+export const Default = {}
+
+export const ReferralEntry = {
+  parameters: {
+    nextjs: {
+      navigation: {
+        pathname: '/login',
+        query: { ref: 'storybook-referral' },
+      },
+    },
+  },
+}
