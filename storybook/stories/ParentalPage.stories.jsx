271834426b816ef259aa09335427c6ffd252d4ff
Hirota Satoshi <202610880@edu.nishiyamato.ed.jp>
2026-09-21 08:42:08 +0900

    test(ui): add parental payment stories

---
+++ storybook/stories/ParentalPage.stories.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import ParentalPage from '../../app/(player)/parental/page'
+
+const meta = {
+  title: 'Pages/Account/Parental',
+  component: ParentalPage,
+  parameters: {
+    nextjs: { navigation: { pathname: '/parental' } },
+  },
+}
+
+export default meta
+
+export const Unlinked = {
+  parameters: {
+    mockApi: {
+      'GET /api/parental/status': {
+        body: { linked: false, pending_token: null },
+      },
+      'POST /api/parental/request': {
+        body: { token: 'storybook-parental-token' },
+      },
+    },
+  },
+}
+
+export const PendingApproval = {
+  parameters: {
+    mockApi: {
+      'GET /api/parental/status': {
+        body: { linked: false, pending_token: 'storybook-parental-token' },
+      },
+    },
+  },
+}
+
+export const Linked = {
+  parameters: {
+    mockApi: {
+      'GET /api/parental/status': {
+        body: { linked: true, pending_token: null },
+      },
+    },
+  },
+}
