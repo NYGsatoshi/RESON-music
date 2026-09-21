062d5f7aca697721ec38a7ea67ae24beee0b428d
Hirota Satoshi <202610880@edu.nishiyamato.ed.jp>
2026-09-21 10:32:43 +0900

    fix(ui): allow home main flex item to shrink on mobile

--- app/(player)/home/page.tsx
+++ app/(player)/home/page.tsx
@@ -151,7 +151,7 @@ export default function PlayerPage() {
       </aside>
 
       {/* メイン */}
-      <main className="flex-1 px-4 pb-24 pt-6 sm:px-8">
+      <main className="min-w-0 flex-1 px-4 pb-24 pt-6 sm:px-8">
         <div className="mx-auto max-w-5xl space-y-10">
           {/* トップバー */}
           <div className="flex flex-wrap items-center gap-2 sm:flex-nowrap sm:gap-4">
